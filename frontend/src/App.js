5fde2a0059b8e129739f774fb063267a2068f71d
Phi Phúc Huỳnh <phiphuc27@gmail.com>
2021-05-11 17:08:49 +0700

    Add HomePage and Product Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
+
 const App = () => {
-    return (
-        <>
-            <h1>Welcome To ProShop</h1>
-        </>
-    );
+  return (
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomePage} exact />
+          <Route path='/product/:id' component={ProductPage} exact />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  );
 };
 
 export default App;
